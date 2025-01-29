5435924d6e686bce82a7c5e034fa07905fdc7943
Jack Kiely <20068129@mail.wit.ie>
2025-01-29 10:53:27 +0000

    Delete a task

--- jacktask/src/App.jsx
+++ jacktask/src/App.jsx
@@ -20,6 +20,13 @@ function App() {
     console.log(`${taskIndex} ${tasks[taskIndex].done}`);
   }
 
+  const deleteHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks.splice(taskIndex, 1);
+    setTaskState({tasks});
+  } 
+
+
   return (
     <div className="container">
       <h1>Tasky</h1>
@@ -32,8 +39,12 @@ function App() {
       key={task.id}
       done={task.done}
       markDone={() => doneHandler(index)}
+      deleteTask = {() => deleteHandler(index)}
+
+     
     />
-    ))} 
+    ))
+    } 
     </div>
   );
 }
