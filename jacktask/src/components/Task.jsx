28c1d1dfdf4cb4d4357c41eb44b7946a04af84d2
Jack Kiely <20068129@mail.wit.ie>
2025-01-29 16:50:04 +0000

    Tasky Lab 2b - Exercises done

--- jacktask/src/components/Task.jsx
+++ jacktask/src/components/Task.jsx
@@ -4,13 +4,13 @@ const Task = (props) => {
     
     return (       
        <div className="card" style={{backgroundColor: props.done ? 'lightgrey' : '#5bb4c4'}}>
+    
             <p className="title">{props.title}</p>
             <p>Due: {props.deadline}</p>
             <p className="description"> {props.description}</p>
-            <p className='priority'> {props.priority}</p>
+            <p className='priority' style={{backgroundColor: props.priority === "Low" ? 'green' : props.priority === "Medium" ? 'orange' : 'red'}}> {props.priority}</p>
             <button onClick={props.markDone} className='doneButton'>Done</button>
             <button className='deleteButton' onClick={props.deleteTask}>Delete</button>
-
         </div>
     )
 
